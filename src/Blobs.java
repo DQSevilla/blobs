e043eda4b0638c90138898e8a89e382a6b4b07e0
David Sevilla <sevilladavid83@gmail.com>
2016-12-01 22:57:24 -0500

    Initial commit

---
+++ src/Blobs.java
@@ -0,0 +1,18 @@
+/* Blobs.java
+ * 
+ * I pledge my honor that I have abided by the Stevens Honor System.
+ * David Sevilla
+ */
+
+/**
+ * Main class for implementing the game Blobs.
+ *
+ * @author David Sevilla
+ * @version 1.0
+ * @since 12/01/2016
+ */
+public class Blobs {
+    public static void main(String[] args) {
+        System.out.println("Blobs");
+    }
+}
